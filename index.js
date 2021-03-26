76f759e5d39c1350b079aaf54a6eff59def7ad09
NikitaIskorka <67067414+NikitaIskorka@users.noreply.github.com>
2021-03-26 23:31:27 +0100

    Сделал задание все кроме перелючения фото по стрелкам

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const lightBoxImageRef = document.querySelector('.lightbox__image');
 const lightBoxClosebtn = document.querySelector(
   '[data-action="close-lightbox"]',
 );
+const lightBoxOverlay = document.querySelector('.lightbox__overlay');
 
 galleryContainerRef.insertAdjacentHTML(
   'beforeend',
@@ -40,8 +41,39 @@ galleryContainerRef.addEventListener('click', event => {
   }
   lightBoxRef.classList.add('is-open');
   lightBoxImageRef.src = event.target.dataset.source;
+  lightBoxImageRef.alt = event.target.alt;
 });
-lightBoxClosebtn.addEventListener('click', event => {
-  lightBoxRef.classList.remove('is-open');
-  lightBoxImageRef.src = '';
+// lightBoxClosebtn.addEventListener('click', () => {
+//   removeClassIsOpen(lightBoxRef);
+//   clearSrcOfImage(lightBoxImageRef);
+// });
+// lightBoxOverlay.addEventListener('click', event => {
+//   if (event.target === lightBoxOverlay) {
+//     removeClassIsOpen(lightBoxRef);
+//     clearSrcOfImage(lightBoxImageRef);
+//   }
+// });
+window.addEventListener('keydown', event => {
+  if (event.code === 'Escape') {
+    removeClassIsOpen(lightBoxRef);
+    clearSrcOfImage(lightBoxImageRef);
+  }
 });
+
+lightBoxRef.addEventListener('click', closeLightbox, lightBoxRef);
+
+function closeLightbox(event) {
+  if (event.target.nodeName !== 'IMG') {
+    removeClassIsOpen(lightBoxRef);
+    clearSrcOfImage(lightBoxImageRef);
+  }
+}
+
+function removeClassIsOpen(lightBox) {
+  lightBox.classList.remove('is-open');
+}
+function clearSrcOfImage(image) {
+  image.src = '';
+}
+
+console.log(galleryContainerRef.children);
